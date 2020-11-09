545c5e4598426a50be6879fbbd4a66978f7f6ab2
ajey potdar <ajey.potdar@gmail.com>
2020-11-10 01:40:27 +0530

    login, set up jwt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,17 @@ class Api::V1::UsersController < ApplicationController
       render json: user.errors, status: :bad_request
     end
   end
+
+  def login
+    user = User.find_by(email: params[:email].to_s.downcase)
+  
+    if user && user.authenticate(params[:password])
+        auth_token = JsonWebToken.encode({user_id: user.id})
+        render json: {auth_token: auth_token}, status: :ok
+    else
+      render json: {error: 'Invalid username / password'}, status: :unauthorized
+    end
+  end
   
   private
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: :create
+      resources :users, only: :create do
+        collection do
+          post 'confirm'
+          post 'login'
+        end
+      end
     end
   end
 end
