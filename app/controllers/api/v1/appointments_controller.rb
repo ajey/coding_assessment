652d824f5d8d7f7a31617184b19393580a16aec6
ajey potdar <ajey.potdar@gmail.com>
2020-11-10 14:54:01 +0530

    validate if slot is available

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::AppointmentsController < ApplicationController
-
   before_action :require_login
 
   def create
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,14 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
   belongs_to :time_slot
+
+  validate :slot_available
+
+  private
+
+  def slot_available
+    if doctor.appointments.where(time_slot_id: time_slot_id).exists?
+      errors.add(:appointment, 'This time slot is already booked')
+    end
+  end
 end
